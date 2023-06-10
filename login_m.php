8cf5677936e9f79252859242754a1210d396d329
Miguel Carlo Flores <95689181+MFlores01@users.noreply.github.com>
2023-06-10 17:07:01 +0800

    Add files via upload

--- login_m.php
+++ login_m.php
@@ -8,27 +8,34 @@ $error = "Username or Password is invalid";
 }
 else
 {
-// Define $username and $password
-$username=$_POST['username'];
-$password=$_POST['password'];
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
 require 'connection.php';
 $conn = Connect();
 
-// SQL query to fetch information of registerd users and finds user match.
-$query = "SELECT username, password FROM MANAGER WHERE username=? AND password=? LIMIT 1";
+// Define $username and $password
+$username = mysqli_real_escape_string($conn, $_POST['username']);
+$password = $_POST['password'];
 
+// SQL query to fetch information of registered users and finds user match.
+$query = "SELECT username, password FROM MANAGER WHERE username=? LIMIT 1";
+
+// To protect MySQL injection for Security purpose
 $stmt = $conn->prepare($query);
-$stmt -> bind_param("ss", $username, $password);
-$stmt -> execute();
-$stmt -> bind_result($username, $password);
-$stmt -> store_result();
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$stmt->bind_result($username, $hashed_password);
+$stmt->store_result();
 
 if ($stmt->fetch())  
 {
-	$_SESSION['login_user1']=$username; // Initializing Session
-	header("location: mybakery.php"); // Redirecting To Other Page
+    if (password_verify($password, $hashed_password)) {
+        $_SESSION['login_user1']=$username; // Initializing Session
+        header("location: mybakery.php"); // Redirecting To Other Page
+    } else {
+        $error = "Username or Password is invalid";
+    }
 } else {
-$error = "Username or Password is invalid";
+    $error = "Username or Password is invalid";
 }
 mysqli_close($conn); // Closing Connection
 }
